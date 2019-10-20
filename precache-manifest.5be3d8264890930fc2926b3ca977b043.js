82380a8462547a0e7859294c05478aedbcf7ada1
Matthew Holloway <matthew@springload.co.nz>
2019-10-20 18:23:44 +1300

    Updates

---
+++ precache-manifest.5be3d8264890930fc2926b3ca977b043.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600def69fc5ef6d2d23569da090f237a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8421e9af4b051f306c2f",
+    "url": "/static/js/2.5a014e15.chunk.js"
+  },
+  {
+    "revision": "c2d6532c43de0abe4b3f",
+    "url": "/static/js/main.ed374cd9.chunk.js"
+  },
+  {
+    "revision": "76d19abf2a46ed4bddd3",
+    "url": "/static/js/runtime-main.b109e48d.js"
+  }
+]);
